13bb4972e252fdc428074f9783f57ea66b0b7dea
David Zachary Milewicz <milewicz@seas.upenn.edu>
2018-11-29 15:26:39 -0500

    p

--- cmake/configs/nuttx_px4fmu-v3_default.cmake
+++ cmake/configs/nuttx_px4fmu-v3_default.cmake
@@ -117,6 +117,7 @@ set(config_module_list
 	#
 	modules/fw_att_control
 	modules/fw_pos_control_l1
+	#modules/pw_pos_control
 	modules/gnd_att_control
 	modules/gnd_pos_control
 	modules/mc_att_control
